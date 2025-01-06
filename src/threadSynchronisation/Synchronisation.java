3ce6bfe30270354b00756106358aabd113f88980
LizzzG <xiguan710@gmail.com>
2025-01-05 23:51:19 -0500

    synchronization

--- src/threadSynchronisation/Synchronisation.java
+++ src/threadSynchronisation/Synchronisation.java
@@ -1,4 +1,33 @@
 package threadSynchronisation;
 
 public class Synchronisation {
+    private static int count = 0;
+    public static void main(String[] args) {
+        Thread one = new Thread(() -> {
+            for(int i = 0; i < 10000; i++) {
+                increment();
+            }
+        });
+        Thread two = new Thread(() -> {
+            for(int i = 0; i < 10000; i++) {
+                increment();
+            }
+        });
+
+        one.start();
+        two.start();
+
+        try{
+            one.join();
+            two.join();
+        }catch(InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(count);
+    }
+
+    private synchronized static void increment() {
+        count++;
+    }
 }
